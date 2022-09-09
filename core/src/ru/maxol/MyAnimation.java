ebc99657f11c9be6e89e06eda3c9ab7ce0f51a94
Maksim Shelepov <34706086+Loredan15@users.noreply.github.com>
2022-09-09 23:51:12 +0300

    Первые пробы пера: - animation sprite разбираем на TextureRegion - научился создавать анимацию

---
+++ core/src/ru/maxol/MyAnimation.java
@@ -0,0 +1,36 @@
+package ru.maxol;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class MyAnimation {
+
+    Texture texture;
+    TextureRegion[][] region;
+    Animation<TextureRegion> animation;
+    float time;
+
+    public MyAnimation(String pathToFile, int col, int row, float fps) {
+        texture = new Texture(pathToFile);
+        region = TextureRegion.split(texture, texture.getWidth() / col, texture.getHeight() / row);
+        TextureRegion[] regions = new TextureRegion[col * row];
+        int count = 0;
+        for (TextureRegion[] textureRegions : region) {
+            for (TextureRegion textureRegion : textureRegions) {
+                regions[count++] = textureRegion;
+            }
+        }
+        animation = new Animation<>(1 / fps, regions);
+    }
+
+    public TextureRegion getFrame(){
+        time += Gdx.graphics.getDeltaTime();
+        return animation.getKeyFrame(time, true);
+    }
+
+    public void dispose(){
+        texture.dispose();
+    }
+}
